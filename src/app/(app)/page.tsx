75d0396b870235e911460b92727391425d1f4db5
ankanmal <ankanlogin@gmail.com>
2024-10-04 09:54:57 +0530

    Added apos to heading

--- src/app/(app)/page.tsx
+++ src/app/(app)/page.tsx
@@ -21,7 +21,7 @@ export default function Home() {
       <main className="flex-grow flex flex-col items-center justify-center px-4 md:px-24 py-12 bg-gray-800 text-white">
         <section className="text-center mb-8 md:mb-12">
           <h1 className="text-3xl md:text-5xl font-bold">
-            Dive into the World of Secret Message's
+            Dive into the World of Secret Message&apos;s
           </h1>
           <p className="mt-3 md:mt-4 text-base md:text-lg">
             True Feedback - Where your identity remains a secret.
